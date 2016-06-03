6783f72c308af3144646ed485b325fc5d783bbc5
Sarah <sarahmichellemeyer@gmail.com>
2016-06-03 16:43:42 -0600

    added three ablum covers to main page. not random yet

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-var userChoose = document.getElementById('userButton');
+$.get("https://lit-fortress-6467.herokuapp.com/object", function(data, status){
+  console.log(data.results);
+  $('.left').append('<div>').addClass('randomAlbum');
+  for (var i = 0; i < 3; i++) {
+    $('.randomAlbum').append(`<img class="albumImg" src='images/${data.results[i].cover_art}' />`);
+    var idCount = 1;
+    $('img').each(function() {
+      $(this).attr('id', 'img' + idCount);
+      idCount++;
+    });
+  }
+ });
