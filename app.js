6bc855f7562a6d8d7012a8abe6937ef94ed83b06
Sarah <sarahmichellemeyer@gmail.com>
2016-06-05 19:58:04 -0600

    Added album covers to playlist page

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 $.get("https://lit-fortress-6467.herokuapp.com/object", function(data, status){
-  console.log(data.results);
   $('.left').append('<div>').addClass('randomAlbum');
   for (var i = 0; i < 3; i++) {
     $('.randomAlbum').append(`<img class="albumImg" src='images/${data.results[i].cover_art}' />`);
@@ -7,6 +6,17 @@ $.get("https://lit-fortress-6467.herokuapp.com/object", function(data, status){
     $('img').each(function() {
       $(this).attr('id', 'img' + idCount);
       idCount++;
-    });
+    })
   }
+  for (var i = 0; i < data.results.length; i++) {
+    $('#albums').append(`<img class="albumBin" src='images/${data.results[i].cover_art}' />`);
+    var idCount2 = 1;
+    $('img').each(function() {
+      $(this).attr('id', 'alb' + idCount2);
+      idCount2++;
+    })
+  }
+  // $("img.albumBin").click(function () {
+  //   $('.textBin').append()
+  //    });
  });
