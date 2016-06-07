7689eee8194906e19a5fa36cf97ea0610a1d17d2
Sarah <sarahmichellemeyer@gmail.com>
2016-06-07 16:40:38 -0600

    added random images to main page

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ $(document).ready(function(){
   $.get("https://api.spotify.com/v1/artists/26dSoYclwsYLMAKD3tpOr4/albums", function(data, status){
     var albums = data.items;
     console.log(data.items);
-      albums.forEach(function(item){
-          $('.left').append('<img class="albumImg" src=' + item.images[1].url + '>');
+    var imageArray = [];
+        for (var i = 0; i < albums.length; i++) {
+            imageArray.push(albums[i].images[1].url);
+            }
+      for (var i = 0; i < 3; i++) {
+          $('.left').append('<img class="albumImg" src=' + imageArray[Math.floor(Math.random() * imageArray.length)] + '>');
             var idCount = 1;
             $('img').each(function() {
               $(this).attr('id', 'img' + idCount);
               idCount++;
               })
-            })
-
+            }
             //********playlist Page********//
         albums.forEach(function(item){
       $('#albums').append('<img class="albumBin" id=' + item.id + ' src=' + item.images[1].url + '>');
