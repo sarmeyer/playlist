9dfb8f303b66346a8574e9ad690ea82965cfbede
Sarah <sarahmichellemeyer@gmail.com>
2016-06-08 17:29:26 -0600

    Reworking all JS, adding artists to text div on playlist page

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 $(document).ready(function(){
-  $.get("https://api.spotify.com/v1/artists/26dSoYclwsYLMAKD3tpOr4/albums", function(data, status){
-    var albums = data.items;
-    var imageArray = [];
-        for (var i = 0; i < albums.length; i++) {
-            imageArray.push(albums[i].images[1].url);
-            }
-      for (var i = 0; i < 3; i++) {
-          $('.left').append('<img class="albumImg" src=' + imageArray[Math.floor(Math.random() * imageArray.length)] + '>');
-            var idCount = 1;
-            $('img').each(function() {
-              $(this).attr('id', 'img' + idCount);
-              idCount++;
-              })
-            }
-        albums.forEach(function(item){
-      $('#albums').append('<img class="albumBin" id=' + item.id + ' src=' + item.images[1].url + '>');
-      $('.albumBin').on("click",function(){
-        if(item.id === this.id) {
-        $('#textBin').append('<p>' + item.name + '</p>');
-          }
+
+    var inputs = $('input')
+    var userInput = inputs[0];
+    var searchButton = inputs[1];
+    var clear = inputs[2];
+    var submit = inputs[3];
+
+  $(searchButton).on('click', makeRequest);
+
+  function makeRequest(userInput){
+    $.get(`https://api.spotify.com/v1/search?q=Britney&type=artist`, function(data, status){
+    getArtists(data.artists.items);
       })
-      $('#clear').on('click',function(){
-        $('#textBin').empty()
-          })
-        });
+    }
+
+  function getArtists(results){
+    console.log(results);
+    results.forEach(function(result){
+      var artists = result.name;
+      $('#textBin').append('<p>' + artists + '</p>');
       })
-    });
+    }
+
+
+});
