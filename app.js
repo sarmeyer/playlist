1256350e614586559132c1bc7a4bbc463e13c62f
Sarah <sarahmichellemeyer@gmail.com>
2016-06-09 15:37:57 -0600

    filtered albums for duplicates and mis-capitalization

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     // var submit = $inputs[3];
 
   $searchButton.on('click', makeRequest);
-  $(document).on('click','.artists',getArt);
+  $(document).on('click','.artists', getArt);
 
 
   function makeRequest(userInput){
@@ -19,21 +19,28 @@ $(document).ready(function(){
     artists.forEach(function(result){
       var artistID = result.id;
       var name = document.createElement('h2');
-      $('h2').addClass('artists').attr('id',artistID)
+      $(name).addClass('artists');
+      name.id = artistID;
       name.innerHTML = result.name;
       $('#textBin').append(name);
       })
     }
     function getArt(albums) {
-      $.get('https://api.spotify.com/v1/artists/'+ $(this).attr("id") +'/albums', function(data,status) {
+      $.get('https://api.spotify.com/v1/artists/'+ this.id +'/albums', function(data,status) {
         showArt(data.items)
       })
     }
     function showArt(albums) {
-      $('#textBin').empty();
+      $('#albums').empty();
+      var duplicates = [];
       albums.forEach(function(album){
-        var albumUrl = album.images[1].url;
-        $('#albums').append('<img src=' + albumUrl + '>');
+        var results = album.name.toLowerCase();
+        if(duplicates.indexOf(results) === -1) {
+          duplicates.push(results)
+          var albumUrl = album.images[1].url;
+          $('#albums').append('<img src=' + albumUrl + '>');
+        }
+        console.log(duplicates);
       })
    }
 
