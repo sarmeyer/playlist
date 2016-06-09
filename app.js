4c3b287ec9f4343caf0ff3bcc4344f3b4c0a31a8
Sarah <sarahmichellemeyer@gmail.com>
2016-06-09 14:17:52 -0600

    Adds artists name to text box and when clicked, adds artists albums to album div, which scrolls

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
 $(document).ready(function(){
 
-    var inputs = $('input')
-    var userInput = inputs[0];
-    var searchButton = inputs[1];
-    var clear = inputs[2];
-    var submit = inputs[3];
+    var $searchButton = $('#searchButton');
+    // var clear = $inputs[2];
+    // var submit = $inputs[3];
 
-  $(searchButton).on('click', makeRequest);
+  $searchButton.on('click', makeRequest);
+  $(document).on('click','.artists',getArt);
+
 
   function makeRequest(userInput){
-    $.get(`https://api.spotify.com/v1/search?q=Britney&type=artist`, function(data, status){
-    getArtists(data.artists.items);
+    var userInput = $('#searchText').val();
+    $.get(`https://api.spotify.com/v1/search?q=${userInput}&type=artist`, function(data, status){
+    showArtist(data.artists.items);
       })
     }
-
-  function getArtists(results){
-    console.log(results);
-    results.forEach(function(result){
-      var artists = result.name;
-      $('#textBin').append('<p>' + artists + '</p>');
+  function showArtist(artists){
+    $('#textBin').empty();
+    artists.forEach(function(result){
+      var artistID = result.id;
+      var name = document.createElement('h2');
+      $('h2').addClass('artists').attr('id',artistID)
+      name.innerHTML = result.name;
+      $('#textBin').append(name);
       })
     }
-
+    function getArt(albums) {
+      $.get('https://api.spotify.com/v1/artists/'+ $(this).attr("id") +'/albums', function(data,status) {
+        showArt(data.items)
+      })
+    }
+    function showArt(albums) {
+      $('#textBin').empty();
+      albums.forEach(function(album){
+        var albumUrl = album.images[1].url;
+        $('#albums').append('<img src=' + albumUrl + '>');
+      })
+   }
 
 });
